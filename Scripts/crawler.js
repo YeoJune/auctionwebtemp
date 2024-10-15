a3e5080ed9dddf29cce22b849c077222b072c142
YeoJune <joyyoj1@naver.com>
2024-10-16 02:23:35 +0900

    test42-5

--- Scripts/crawler.js
+++ Scripts/crawler.js
@@ -652,10 +652,11 @@ class BrandAuctionCrawler extends Crawler {
         });
       });
       await this.waitForLoading(page);
+      console.log(await page.$(this.config.crawlSelectors.itemContainer));
       await page.click(this.config.crawlSelectors.itemContainer);
       const newPage = await newPagePromise;
       await this.initPage(newPage);
-      await this.sleep(500);
+      await this.sleep(100);
       await this.waitForLoading(newPage);
 
       let item;
