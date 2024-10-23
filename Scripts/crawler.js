50589e26285eb7050bfa6707baafea09acf837c6
YeoJune <joyyoj1@naver.com>
2024-10-23 20:13:18 +0900

    temp83-6

--- Scripts/crawler.js
+++ Scripts/crawler.js
@@ -238,8 +238,10 @@ class Crawler {
         page.type(this.config.signinSelectors.userId, this.config.loginData.userId),
         page.type(this.config.signinSelectors.password, this.config.loginData.password),
       ]);
-      await page.click(this.config.signinSelectors.loginButton);
-      await page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: this.pageTimeout });
+      await Promise.all([
+        await page.click(this.config.signinSelectors.loginButton),
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: this.pageTimeout }),
+      ]);
     });
   }
   async loginCheckCrawler() {
