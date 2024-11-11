997efa968b1d8b77ad2f098aac76371063cdb5af
YeoJune <joyyoj1@naver.com>
2024-11-11 21:06:18 +0900

    [20241111] puppeteer userData2

--- Scripts/crawler.js
+++ Scripts/crawler.js
@@ -294,7 +294,7 @@ class Crawler {
     const browser = await puppeteer.launch({
       executablePath: '/usr/bin/chromium-browser',
       headless: 'true',
-      userDataDir: '~/puppeteer-user-data',
+      userDataDir: '../puppeteer-user-data',
       args: [
         '--incognito',
         '--no-sandbox',
