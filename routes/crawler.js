35354e25b7d913238e3f79aae91f676682b0b67f
YeoJoon <joyyoj1@naver.com>
2025-03-20 02:51:25 +0900

    TEMP

--- routes/crawler.js
+++ routes/crawler.js
@@ -68,11 +68,9 @@ async function crawlAll() {
       );
 
       isCrawling = true;
-      let ecoAucItems = await ecoAucCrawler.crawlAllItems(existingEcoAucIds);
-      let brandAucItems = await brandAucCrawler.crawlAllItems(
-        existingBrandAuctionIds
-      );
-      let starAucItems = await starAucCrawler.crawlAllItems(existingStarAucIds);
+      let ecoAucItems = await ecoAucCrawler.crawlAllItems();
+      let brandAucItems = await brandAucCrawler.crawlAllItems();
+      let starAucItems = await starAucCrawler.crawlAllItems();
 
       if (!ecoAucItems) ecoAucItems = [];
       if (!brandAucItems) brandAucItems = [];
