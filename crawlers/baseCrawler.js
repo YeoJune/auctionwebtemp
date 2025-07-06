f1a9bcd5fbc4d0541830266e49e756635a17c46d
YeoJoon <joyyoj1@naver.com>
2025-07-07 06:26:47 +0900

    session

--- crawlers/baseCrawler.js
+++ crawlers/baseCrawler.js
@@ -361,7 +361,7 @@ class AxiosCrawler {
 
     this.isLoggedIn = false;
     this.loginTime = null;
-    this.sessionTimeout = 1000 * 60 * 60 * 3; // 3시간
+    this.sessionTimeout = 1000 * 60 * 60 * 3;
     this.maxRetries = 3;
     this.retryDelay = 1000;
 
@@ -382,6 +382,15 @@ class AxiosCrawler {
 
   // 로그인 체크 기본 메서드 (각 크롤러에서 오버라이드)
   async loginCheck() {
+    // 세션이 만료된 경우 무조건 false 반환
+    if (!this.isSessionValid()) {
+      console.log("Session expired, forcing logout");
+      this.lastLoginCheck = Date.now();
+      this.lastLoginCheckResult = false;
+      this.isLoggedIn = false;
+      return false;
+    }
+
     // 마지막 로그인 체크 시간이 5분 이내면 캐시된 결과 반환
     if (
       this.lastLoginCheck &&
