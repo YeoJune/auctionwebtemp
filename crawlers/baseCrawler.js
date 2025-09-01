6fb08f3909048903996f81ccc62ed22e204bf8d9
YeoJoon <joyyoj1@naver.com>
2025-09-02 00:19:32 +0900

    3 sec test

--- crawlers/baseCrawler.js
+++ crawlers/baseCrawler.js
@@ -30,7 +30,7 @@ class AxiosCrawler {
           Accept:
             "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         },
-        timeout: 30000,
+        timeout: 3 * 1000,
         maxRedirects: 5,
       });
 
