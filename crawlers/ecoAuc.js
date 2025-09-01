d70a521e41b746a594592340cbc7f15f2798b31b
YeoJoon <joyyoj1@naver.com>
2025-09-02 00:29:07 +0900

    hmm

--- crawlers/ecoAuc.js
+++ crawlers/ecoAuc.js
@@ -1121,7 +1121,7 @@ class EcoAucCrawler extends AxiosCrawler {
       const bidType = "direct";
       const url = this.config.detailUrl(itemId, bidType);
 
-      const response = await clientInfo.client.get(url, { timeout: 1500 });
+      const response = await clientInfo.client.get(url, { timeout: 5 * 1000 });
       const $ = cheerio.load(response.data, { xmlMode: true });
 
       // Price 추출
