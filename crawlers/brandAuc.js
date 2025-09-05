404e46433233bdb3f4dd6a6c12053a275a67b203
YeoJoon <joyyoj1@naver.com>
2025-09-05 20:28:35 +0900

    20 test

--- crawlers/brandAuc.js
+++ crawlers/brandAuc.js
@@ -8,8 +8,8 @@ let pLimit;
   pLimit = (await import("p-limit")).default;
 })();
 
-const LIMIT1 = 10;
-const LIMIT2 = 10;
+const LIMIT1 = 20;
+const LIMIT2 = 20;
 
 const brandAucConfig = {
   name: "BrandAuc",
