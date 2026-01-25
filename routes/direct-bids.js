f6865a2d1f9a660b42ee974806a9220b17440419
YeoJoon <joyyoj1@naver.com>
2026-01-26 06:02:11 +0900

    schedule

--- routes/direct-bids.js
+++ routes/direct-bids.js
@@ -1119,10 +1119,6 @@ const startAutoSubmitScheduler = () => {
   }, AUTO_SUBMIT_INTERVAL);
 };
 
-if (process.env.ENV !== "development") {
-} else {
-  // 라우터 초기화 시 스케줄러 시작
-  startAutoSubmitScheduler();
-}
+startAutoSubmitScheduler();
 
 module.exports = router;
